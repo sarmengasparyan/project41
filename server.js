315bf459e11b7f4b74e1e42f8f1ab564dd5560df
user <user@lab1556.tumo.lab>
2017-09-24 17:15:36 +0400

    second commit

--- server.js
+++ server.js
@@ -6,18 +6,21 @@ const basicAuth = require('express-basic-auth')
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3000;
- 
-// server.use(basicAuth({
-//     users: { 'admin': 'secret' },
-//     challenge: true,
-//     unauthorizedResponse: getUnauthorizedResponse,
-// }))
 
-// function getUnauthorizedResponse(req) {
-//     return req.auth ?
-//         {'Unauthorized':('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')} : 
-//         {'Unauthorized':'No credentials provided'}
-// }
+
+
+
+ server.use(basicAuth({
+     users: { 'admin': 'secret' },
+     challenge: true,
+    unauthorizedResponse: getUnauthorizedResponse,
+}))
+
+ function getUnauthorizedResponse(req) {
+     return req.auth ?
+         {'Unauthorized':('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')} : 
+         {'Unauthorized':'No credentials provided'}
+}
 
 server.use(middlewares)
 server.use(router)
